3ee0ee4c5a957f5f7d69f1b05c63aaaedd9f0c86
Luka Siric <siric.luka@gmail.com>
2020-04-29 18:14:54 +0200

    real world duration

--- scripts/config.js
+++ scripts/config.js
@@ -4,7 +4,7 @@ createjs.Sound.registerSound({ src: "audio/countdown.mp3", id: "countdown" });
 
 const SECOND = 1000;
 const MINUTE = 60 * SECOND;
-const STATION_DURATION = 7 * SECOND;
+const STATION_DURATION = 60 * SECOND;
 const PAUSE_DURATION = 16 * SECOND;
 const REST_DURATION = 2 * MINUTE + 1 * SECOND;
 const SERIES_NUMBER = 3;
